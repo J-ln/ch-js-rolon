e2123873090c2440b8125086a8be3ea15bdcc168
Julian Rolon <julianrolon7@gmail.com>
2021-07-29 15:40:38 -0300

    init

---
+++ js/clase2.js
@@ -0,0 +1,18 @@
+let edad = parseInt(prompt("Ingrese su edad: "));
+if (edad < 18) {
+    alert("Tas chikit@ xD");
+} else if (edad >= 60) {
+    alert("Llegando a la salida -->");
+} else if (edad >= 50) {
+    alert("Como llegaste hasta aca?");
+} else if (edad >= 40) {
+    alert("Poco pelo");
+} else if (edad >= 30) {
+    alert("Y esas canas?");
+} else if (edad >= 25) {
+    alert("Segui que falta");
+} else if (edad >= 18) {
+    alert("Welcome to Hell");
+} else {
+    alert("Ingresa un numero por favor");
+}
