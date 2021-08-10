e471fe6966d19863f681ab0ebf20fd5225ce17a0
Julian Rolon <julianrolon7@gmail.com>
2021-08-10 14:42:23 -0300

    clase4

---
+++ js/clase4.js
@@ -0,0 +1,53 @@
+//
+
+function numeroAleatorio() {
+    let randomN = Math.floor(Math.random() * 10) + 1;
+    return randomN;
+}
+
+function compare(userN, RandN) {
+    if (userN == RandN) {
+        alert(
+            "Felicitaciones! Acertaste! Podes sumar esto como uno de los grandes logros de tu vida :D."
+        );
+        return true;
+    } else {
+        alert("Casi! Pero no es el numero.");
+        return false;
+    }
+}
+
+function game() {
+    let number = numeroAleatorio();
+    let userTry = parseInt(
+        prompt(
+            "Adivina que numero estoy pensando. Ingresa un numero del 1 al 10. Tenes 3 intentos:"
+        )
+    );
+    let tries = 2;
+    let answer = compare(userTry, number);
+    while (answer == false && tries != 0) {
+        userTry = parseInt(prompt("Ingresa otro numero:"));
+        answer = compare(userTry, number);
+        tries--;
+    }
+    if (answer == false) {
+        alert("Perdiste! Perdiste! No hay nadie peor que vos! :C .");
+    }
+}
+
+function randomNumberGame() {
+    let start = prompt(
+        'Hola. Escribe "si" para empezar el juego o "no" para salir:'
+    );
+    switch (start) {
+        default:
+            alert("Ingresaste una opcion incorrecta.");
+        case "si":
+            game();
+        case "no":
+            break;
+    }
+}
+
+randomNumberGame();
